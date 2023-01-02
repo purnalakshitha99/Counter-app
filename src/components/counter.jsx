1feca56121a6d5d68d1d56faeea22697cd20c13c
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-02 23:11:49 +0530

    image added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+  };
   render() {
-    return <h1>Hello Word</h1>;
+    return (
+      <div>
+        <img src={this.state.imageUrl} alt="photo"></img>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>;
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 

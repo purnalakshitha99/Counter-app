c3a27753e7d826ff67e181cb9995c0932c865519
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-02 23:17:57 +0530

    badge added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,12 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200",
   };
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="photo"></img>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>;
+        <span className="badge bg-primary m-2">{this.formatCount()}</span>
+        <button>Increment</button>
       </div>
     );
   }

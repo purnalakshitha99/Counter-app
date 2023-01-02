8bf0de1036e4bb2cc66457b003d002a8766f0da6
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-02 23:28:31 +0530

    buttun styels

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,11 +4,18 @@ class Counter extends Component {
   state = {
     count: 0,
   };
+
+  styles = {
+    fontSize: 12,
+    fontWeight: "bold",
+  };
   render() {
     return (
       <div>
-        <span className="badge bg-primary m-2">{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge bg-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }

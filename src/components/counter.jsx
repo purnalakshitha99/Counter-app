a5670eb86b5190951c66901bf4cabb0f6e87272f
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-04 09:39:27 +0530

    redering list done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tag: ["tag1", "tag2", "tag3"],
   };
 
   // styles = {
@@ -12,12 +13,21 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span style={{ fontSize: 12 }} className="badge bg-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tag.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 text-bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {

4fd209f3551d834cc597d86a0c694a8e2e2a4109
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-06 10:24:51 +0530

    counter variable value added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tag: ["tag1", "tag2", "tag3"],
     //tag: [],
   };
@@ -30,7 +30,7 @@ class Counter extends Component {
 
   handleIncrement = (products) => {
     console.log(products);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   doHandleIncrement = () => {
@@ -55,12 +55,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 text-bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,7 +5,7 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 4 },
-      { id: 2, value: 0 },
+      { id: 2, value: 3 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
@@ -14,7 +14,7 @@ class Counters extends Component {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter key={counter.id} value={counter.value} />
         ))}
       </div>
     );

1a14da19d12c6cae03fd872e554498b26ea529da
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-14 10:19:25 +0530

    stateless functional components added (SFC)

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand h1" href="#">
-            Navbar{" "}
-            <span className="badge text-bg-secondary">
-              {this.props.totalCounters}
-            </span>
-          </a>
-        </div>
-      </nav>
-    );
-  }
-}
+// class NavBar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar bg-body-tertiary">
+//         <div className="container-fluid">
+//           <a className="navbar-brand h1" href="#">
+//             Navbar{" "}
+//             <span className="badge text-bg-secondary">
+//               {this.props.totalCounters}
+//             </span>
+//           </a>
+//         </div>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default NavBar;
+
+//stateless functional components
+const NavBar = (props) => {
+  //methana props dana nisa yata this. eka ain kranw //this.props.totalCounters kiyan eka
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand h1" href="#">
+          Navbar{" "}
+          <span className="badge text-bg-secondary">{props.totalCounters}</span>
+        </a>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;

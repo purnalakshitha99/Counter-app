05e7f65aaa81fc884bfaeb9650bf70b0c92204dc
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2023-01-14 07:47:42 +0530

    navbar bootstrap copy and paste

---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar bg-body-tertiary">
+        <a className="vbar-brand" href="#">
+          Navbar
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import Counters from "./components/counters";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>
 );
 
